fc837df70717387400bc18b7545ab058774631b2
andan <2864319823@qq.com>
2018-04-05 12:08:49 +0800

    Update basic.java

--- basic.java
+++ basic.java
@@ -1,5 +1,6 @@
 // 1,1,2,3,5,8,13...	
 	//f(0) = 1;f(1) = 1;...;f(n) = f(n-1)+f(n-2);
+	//method first,time complexity is O(2^N)
 	public static int fibonacci_Sequence(int n) {//n代表斐波那契数列的第n项
 		if (n < 1) {
 			return 0;
@@ -8,4 +9,23 @@
 		}else {
 			return fibonacci_Sequence( n - 1 ) + fibonacci_Sequence( n - 2 );
 		}
+	}
+	
+	//method second,time complexity is O(N)
+	public static int fibonacci_Sequence(int n) {//n代表斐波那契数列的第n项
+		if (n < 1) {
+			return 0;
+		}
+		if (n==1 || n == 2) {
+			return 1;
+		}
+		int res = 1;
+		int pre = 1;
+		int temp = 0;
+		for(int i = 3; i <= n; i++){
+			tmp = res;
+			res = res + pre;
+			pre = tmp;
+		}
+		return res;
 	}
