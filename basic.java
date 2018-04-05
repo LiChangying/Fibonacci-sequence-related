c47e27381fa210d2a7e442c464e52315aeecc6bf
andan <2864319823@qq.com>
2018-04-05 11:20:51 +0800

    Update basic.java

--- basic.java
+++ basic.java
@@ -1,9 +1,9 @@
 // 1,1,2,3,5,8,13...	
-public static int fibonacci_Sequence(int n) {//n代表斐波那契数列的第n项
-		int num = 0;//return value
-		if (n == 1) {
-			return 1;
-		}else if (n == 2) {
+	//f(0) = 1;f(1) = 1;...;f(n) = f(n-1)+f(n-2);
+	public static int fibonacci_Sequence(int n) {//n代表斐波那契数列的第n项
+		if (n < 1) {
+			return 0;
+		}else if (n==1 || n == 2) {
 			return 1;
 		}else {
 			return fibonacci_Sequence( n - 1 ) + fibonacci_Sequence( n - 2 );
