c8ab6c2ef901c48e5c37b869470dba354a0b0bf7
andan <2864319823@qq.com>
2018-04-05 11:11:46 +0800

    Create basic.java
    recrusion accomplish F(n) = F(n-1)+F(n-2)

---
+++ basic.java
@@ -0,0 +1,10 @@
+	public static int fibonacci_Sequence(int n) {//n代表斐波那契数列的第n项
+		int num = 0;//return value
+		if (n == 1) {
+			return 1;
+		}else if (n == 2) {
+			return 1;
+		}else {
+			return fibonacci_Sequence( n - 1 ) + fibonacci_Sequence( n - 2 );
+		}
+	}
